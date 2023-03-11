54b81afb10e89bba02269b99df704e807065ecde
hebrohim <raheemibrohim@mail.com>
2023-03-11 22:37:45 +0100

    initial commit :Add react boiler plate

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { useState } from "react";
+
+
+const url = " https://course-api.com/react-tours-project"
+function App() {
+
+  return (
+    
+    <div className="App row">
+
+
+    </div>
+  );
+}
+
+export default App;
