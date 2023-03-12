6477d4c1cb67da835e342fde8c2797c5f930348c
hebrohim <raheemibrohim@mail.com>
2023-03-12 18:18:53 +0100

    feat: add tour price and loading effect when fetching data from database

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import ListTours from "./Components/ListTours";
-
 const baseURL = " https://course-api.com/react-tours-project";
 function App() {
   const [Tours, setTours] = useState([]);
 
+
+  const [Loading, setLoading] = useState(true)
   useEffect(() => {
+    setLoading(true)
     axios.get(baseURL).then((response) => {
+      setLoading(false)
       setTours(response.data);
     });
   }, []);
@@ -17,7 +20,9 @@ function App() {
     <div className="App">
         <h2 id="ourTourText">Our Tours</h2>
     <div id = "underline"></div>
-      <ListTours Tours={Tours} />
+    {
+     Loading?<h1>Loading ...</h1>:<ListTours Tours={Tours} />
+    }
     </div>
   );
 }
--- src/Components/ListTours.js
+++ src/Components/ListTours.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 const ListTours = ({ Tours }) => {
   return (
     <div className="wrapper">
@@ -7,13 +6,16 @@ const ListTours = ({ Tours }) => {
         let { id, name, info, image, price } = tour;
         // console.log(name)
         return (
+                    
           <div key={id} className="card">
+   
             <img src={image} alt="name" id="tourImage" />
+            <span id="price">${price}</span>
             <div className="container">
               <h4 id="tourName">
                 <b>{name}</b>
               </h4>
-              {/* <p>Architect & Engineer</p>  */}
+              <p id="info">{info}</p> 
             </div>
           </div>
         );
