69463bc1e59172a7e7d92cb5e9584cf144afe502
hebrohim <raheemibrohim@mail.com>
2023-03-12 17:24:28 +0100

    feat: add media query for responsiveness

--- src/Components/ListTours.js
+++ src/Components/ListTours.js
@@ -2,22 +2,18 @@ import React from "react";
 
 const ListTours = ({ Tours }) => {
   return (
-    <div>
+    <div className="wrapper">
       {Tours.map((tour) => {
         let { id, name, info, image, price } = tour;
         // console.log(name)
         return (
-          <div key={id} className="wrapper">
-            {/* <img src = {image}id="tourImage"/> */}
-
-            <div className="card">
-              <img src={image} alt="name" id="tourImage" />
-              <div className="container">
-                <h4>
-                  <b>{name}</b>
-                </h4>
-                {/* <p>Architect & Engineer</p>  */}
-              </div>
+          <div key={id} className="card">
+            <img src={image} alt="name" id="tourImage" />
+            <div className="container">
+              <h4 id="tourName">
+                <b>{name}</b>
+              </h4>
+              {/* <p>Architect & Engineer</p>  */}
             </div>
           </div>
         );
