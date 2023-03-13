5cb57a16912b1349f7c9726ad4dc0a3e3cc1e225
hebrohim <raheemibrohim@mail.com>
2023-03-13 03:00:25 +0100

    feat:add function to delete and reload new tours

--- src/App.js
+++ src/App.js
@@ -2,30 +2,40 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import ListTours from "./Components/ListTours";
+
 const baseURL = " https://course-api.com/react-tours-project";
-function App() {
+
+const App = () => {
   const [Tours, setTours] = useState([]);
+  //function that deletes Tour
 const deleteTours = (id) =>{
   let newTours = Tours.filter((tour)=>tour.id !== id);
   setTours(newTours)
  
 }
 
+//set loading
   const [Loading, setLoading] = useState(true)
-  useEffect(() => {
+  //function that fetches data from database
+  const fetchData = () => {
     setLoading(true)
     axios.get(baseURL).then((response) => {
       setTours(response.data);
       setLoading(false)
     });
-  }, []);
+  }
 
+  useEffect(fetchData, []);
+  //Reload new Tours 
+  const reloadTours = () =>{
+    fetchData()
+  }
   return (
     <div className="App">
         <h2 id="ourTourText">Our Tours</h2>
     <div id = "underline"></div>
     {
-     Loading?<h1>Loading ...</h1>:<ListTours Tours={Tours} deleteTours = {deleteTours} />
+     Loading?<h1>Loading ...</h1>:<ListTours Tours={Tours} deleteTours = {deleteTours} reloadTours = {reloadTours} />
     }
     </div>
   );
--- src/Components/ListTours.js
+++ src/Components/ListTours.js
@@ -2,11 +2,19 @@ import React,{useState}from "react";
 import Tour from "./Tour";
 
 
-const ListTours = ({ Tours,deleteTours }) => {
+const ListTours = ({ Tours,deleteTours,reloadTours }) => {
+  
+   if (Tours.length == 0) {
+    return <div style={{display:"flex",flexDirection:"column",alignItems:"center"}}>
+ <h1 style={{textAlign:"center"}}>No tours available</h1>
+ <button id="refreshBtn" onClick={reloadTours}>Refresh</button>
+        </div>
     
-
+   }
   return (
     <div className="wrapper">
+
+      
       {Tours.map((tour) => {
         
       return <Tour tour = {tour} key = {tour.id} deleteTours = {deleteTours}/>
